aab3b2a4c78a1047c231f209cdc0045970b5c952
Colurswitch <71611550+Colurswitch@users.noreply.github.com>
2025-04-16 21:41:08 -0400

    fi

---
+++ tvmenu.js
@@ -0,0 +1,53 @@
+class TVMenu {
+    /**
+     * Constructor for TVMenu
+     *
+     * @param {Object} options - Options for the TVMenu
+     * @param {HTMLElement} [options.domElement] - The HTMLElement to use as the panel
+     * @param {TVMenuItem[]} options.items - The array of items to include in the menu.
+     * @constructor
+     */
+    constructor(options) {
+        this.domElement = options.domElement || document.createElement("tvm-panel");
+        this.items = options.items
+    }
+}
+
+class TVMenuItem {
+  /**
+   * Constructor for TVMenuItem
+   * @param {Object} options - Options for the TVMenuItem
+   * @param {String} options.text - The text to display for the item
+   * @param {string} options.icon - Valid HTML to display as an icon. Can be a basic icon or even an image.
+   * @param {String} options.type - The type of item. Must be one of: button, checkbox, enum, folder, or color
+   * @param {Function} [options.onSelect] - The function to call when the item is selected
+   * @param {{text: string, value: string}[]} [options.possibleValues] - An array of valid options for the enum type. Only used if type is enum.
+   * @param {function(string)} [options.onChange] - The function to call when the item's value changes. Won't be called if type is button or folder.
+   * @param {TVMenuItem[]} [options.children] - The array of children items for this item. Only used if type is folder.
+   */
+  constructor(options) {
+    this.text = options.text;
+    if (
+      options.type !== "button" &&
+      options.type !== "checkbox" &&
+      options.type !== "enum" &&
+      options.type !== "folder" &&
+      options.type !== "color"
+    ) {
+      throw new Error("TVMenuItem: type is invalid.");
+    } else if (
+        options.type === "enum" &&
+        !options.possibleValues ||
+        options.possibleValues.length === 0
+      ) {
+      throw new Error("TVMenuItem: possibleValues is invalid.");
+    }
+    this.type = options.type;
+    this.icon = options.icon;
+    this.onSelect = options.onSelect;
+    this.possibleValues = options.possibleValues;
+    this.onChange = options.onChange;
+    this.children = options.children;
+    // Type must be: button, checkbox, enum, folder, or color
+  }
+}
