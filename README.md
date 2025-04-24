f75f92303576f5d8fb6d2ff93e8eb7788d1d5fe5
Colurswitch <71611550+Colurswitch@users.noreply.github.com>
2025-04-23 22:00:30 -0400

    Chunk 1

--- README.md
+++ README.md
@@ -19,36 +19,43 @@ You can change the styling of the menu by adding the following CSS to your ```<h
 ### JavaScript
 Create a new TVMenu instance:
 ```javascript
-    const menu = new TVMenu({
-        [domElement: HTMLElement | null] // DOM element (<tvm-panel>) to contain the menu. By default, the menu will be created inside a new <tvm-panel> element.
-        sections: [ // {title: string, children: TVMenuItem[]}
-            new TVMenuItem({
-                [text: string], // Text to display in the menu item. Required unless type is "separator".
-                [icon: string | HTMLElement | null], // Valid HTML to display as an icon. Required unless type is "separator".
-                type: "button" | "checkbox" | "enum" | "color" | "folder" | "input" | "separator", // Type of menu item.
-                [onSelect: () => void | null], // Callback function to execute when the item has been clicked or selected. Optional but won't be used if type is "separator".
-                [possibleValues: {text: string, value: string}[] | null], // Array of values for the enum type. Required if type is "enum".
-                [onChange: (string) => void | null], // Callback function to execute when the value of the item has changed. Optional but won't be used if type is "separator".
-                [children: TVMenuItem[] | null], // Array of child items. Required if type is "folder".
-                [allowAlpha: boolean], // Allow the user to select the alpha value of the color type. Optional but won't be used if type is "separator".
-                [isPassword: boolean], // Whether the item is a password field. Use only if type is "input".
-                [default: string | null] // Default value for the item. Optional but won't be used if type is "separator", "button", or "folder". Required and must be the number if type is "enum". Must be a valid CSS color string if type is "color". Can be just a string if type is "input". Must evaluate to a boolean if type is checkbox.
-            })
-        ]
-    })
+const menu = new TVMenu({
+    items: [ // Array of menu items. Required.
+        new TVMenuItem({
+            [text: string], // Text to display in the menu item. Required unless type is "separator".
+            [icon: string | HTMLElement | null], // Valid HTML to display as an icon. Required unless type is "separator".
+            type: "button" | "checkbox" | "enum" | "color" | "folder" | "input" | "separator", // Type of menu item.
+            [onSelect: () => void | null], // Callback function to execute when the item has been clicked or selected. Optional but won't be used if type is "separator".
+            [possibleValues: {text: string, value: string}[] | null], // Array of values for the enum type. Required if type is "enum".
+            [onChange: (string) => void | null], // Callback function to execute when the value of the item has changed. Optional but won't be used if type is "separator".
+            [children: TVMenuItem[] | null], // Array of child items. Required if type is "folder".
+            [allowAlpha: boolean], // Allow the user to select the alpha value of the color type. Optional but won't be used if type is "separator".
+            [isPassword: boolean], // Whether the item is a password field. Use only if type is "input".
+            [default: string | null] // Default value for the item. Optional but won't be used if type is "separator", "button", or "folder". Required and must be the number if type is "enum". Must be a valid CSS color string if type is "color". Can be just a string if type is "input". Must evaluate to a boolean if type is checkbox.
+        })
+    ]
+})
 ```
 
 Control the menu:
 ```javascript
-    menu.show() // Show the menu
-    menu.hide() // Hide the menu
-    menu.toggle() // Toggle the menu
+menu.show() // Show the menu
+menu.hide() // Hide the menu
+menu.toggle() // Toggle the menu
 
-    menu.moveSelection("up" | "down") // Move the selection up or down
-    menu.select() // Select the current item
+menu.moveSelection("up" | "down") // Move the selection up or down
+menu.select() // Select the current item
+menu.back() // Go back to the previous item or menu
+```
+
+Display messages:
+```javascript
+menu.alert(message: string) => Promise<void> // Display a generic alert message. You can specify a promise callback if you want to execute code after the user clicks "OK".
+menu.prompt(message: string, defaultValue?: string | number | boolean | "", type: "HTML Input type", onchange?: (string | number | boolean) => void, min?: number | null, max?: number | null) => Promise<string | number | boolean> // Display a prompt message. You can specify a function to execute when the user changes the value of the input. You can specify a minimum and maximum value for the "number" or "range" input. You can specify a promise callback, called with the user's input, if you want to execute code after the user clicks "OK".
+menu.confirm(message: string) => Promise<boolean> // Display a confirmation message. You can specify a promise callback if you want to execute code after the user clicks "Yes" or "No".
 ```
 
 Danger zone:
 ```javascript
-    menu.destroy() // Destroy the menu and remove it from the DOM
+menu.destroy() // Destroy the menu and remove it from the DOM
 ```
--- tvmenu.js
+++ tvmenu.js
@@ -2,61 +2,266 @@ class TVMenu {
     /**
      * Constructor for TVMenu
      *
-     * @param {Object} options - Options for the TVMenu
-     * @param {HTMLElement} [options.domElement] - The HTMLElement to use as the panel
-     * @param {{title: string, children: TVMenuItem[]}[]} options.sections - An array of sections, each containing a title and an array of children items
+     * @param {object} options - Options for the TVMenu
+     * @param {string} options.header - Text to display at the top of the menu
+     * @param {TVMenuItem[]} options.items - The array of items to display in the menu
      */
     constructor(options) {
-        this.domElement = options.domElement || document.createElement("tvm-panel");
-        this.sections = options.sections;
+        this.header = options.header;
+        this.items = options.items;
+
+        this.menuContainer = document.createElement("tvm-panel");
+        this.mainInnerSection = document.createElement("tvm-section");
+        this.dialogContainer = document.createElement("tvm-panel");
+        this.mainInnerDialog = document.createElement("tvm-dialog");
+
+        
+        this.menuContainer.appendChild(this.mainInnerSection);
+    }
+
+    /**
+     * Creates a tree of items in the given container.
+     *
+     * @param {TVMenuItem[]} items - The array of items to create in the container.
+     * @param {HTMLElement} container - The container element to add the items to.
+     * @private
+     */
+    #createItemTree(items, container) {
+        items.forEach((item, idx) => {
+            const $newItem = document.createElement("tvm-item");
+            $newItem.innerHTML = `
+                <span class="icon">
+                    ${item.icon || ""}
+                </span>
+                <div class="right">
+                    <h3>${item.text}</h3>
+                    <h3>${["button","folder","separator"].includes(item.type) || !item.default ? "" : item.default}</h3>
+                </div>
+            `;
+            if (idx == 0) $newItem.classList.add("active");
+            if (item.default) $newItem.dataset.tvm_value = item.default;
+            else {
+                if (item.type == "checkbox") $newItem.dataset.tvm_value = false;
+                else if (item.type == "number") $newItem.dataset.tvm_value = item.min?item.min:0;
+                else if (item.type == "input") $newItem.dataset.tvm_value = "";
+            }
+            if (item.onSelect) $newItem.onclick = (evt) => {
+                if (item.type == "input" && item.onChange) this.prompt(item.text, item.default, item.isPassword ? "password" : "text", item.onChange, item.min, item.max).then(value => item.value = value);
+                if (item.type == "checkbox")
+                item.onSelect(evt);
+            };
+            else if (item.type == "folder") {
+                const _ = document.createElement("tvm-section");
+                $newItem.parentElement.insertAdjacentElement("afterend", _);
+                this.#createItemTree(item.children, _);
+            }
+            container.appendChild($newItem);
+        });
+    }
+
+    /**
+     * Displays an alert dialog with the provided message.
+     *
+     * @param {string} message - The message to display in the alert dialog.
+     * @returns {Promise<void>} A promise that resolves when the dialog is dismissed.
+     */
+    alert(message) {
+        return new Promise((resolve) => {
+            this.mainInnerDialog.innerHTML = `<tvm-dialog-content>${message}</tvm-dialog-content>`;
+            const $newActions = document.createElement("tvm-dialog-actions");
+            const $acceptButton = document.createElement("button");
+            $acceptButton.innerHTML = "OK";
+            $acceptButton.onclick = () => {
+                this.dialogContainer.classList.add("hidden");
+                this.dialogContainer.innerHTML = "";
+                resolve();
+            }
+            $newActions.appendChild($acceptButton);
+            this.mainInnerDialog.appendChild($newActions);
+            this.dialogContainer.appendChild(this.mainInnerDialog);
+        });
+    }
+
+    /**
+     * Displays a prompt dialog with the provided message and options.
+     *
+     * @param {string} message - The message to display in the prompt dialog.
+     * @param {string|number|boolean} [defaultValue=""] - The default value to display in the input field.
+     * @param {string} [type="text"] - The type of input to use in the prompt dialog.
+     * @param {function(string)} [onchange] - A function to call when the value of the input changes.
+     * @param {number} [min] - The minimum value of the input field.
+     * @param {number} [max] - The maximum value of the input field.
+     * @returns {Promise<string>} A promise that resolves with the value of the input when the dialog is dismissed.
+     */
+    prompt(message, defaultValue = "", type = "text", onchange, min, max) {
+        return new Promise((resolve) => {
+            const $newContent = document.createElement("tvm-dialog-content");
+            $newContent.innerHTML = message;
+            const $newInput = document.createElement("input");
+            $newInput.type = type;
+            $newInput.value = defaultValue;
+            if (onchange) $newInput.onchange = () => onchange($newInput.value);
+            if (min) $newInput.min = min;
+            if (max) $newInput.max = max;
+            $newContent.appendChild($newInput);
+            const $newActions = document.createElement("tvm-dialog-actions");
+            const $okButton = document.createElement("button");
+            $okButton.innerHTML = "OK";
+            $okButton.onclick = () => {
+                this.dialogContainer.classList.add("hidden");
+                this.dialogContainer.innerHTML = "";
+                resolve($newInput.value);
+            }
+            const $cancelButton = document.createElement("button");
+            $cancelButton.innerHTML = "Cancel";
+            $cancelButton.onclick = () => {
+                this.dialogContainer.classList.add("hidden");
+                this.dialogContainer.innerHTML = "";
+                resolve(defaultValue);
+            }
+            $newActions.appendChild($okButton);
+            this.mainInnerDialog.appendChild($newContent);
+            this.mainInnerDialog.appendChild($newActions);
+            this.dialogContainer.appendChild(this.mainInnerDialog);
+        })
+    }
+
+    confirm(message) {
+        return new Promise((resolve) => {
+            const $newContent = document.createElement("tvm-dialog-content");
+            $newContent.innerHTML = message;
+            const $newActions = document.createElement("tvm-dialog-actions");
+            const $okButton = document.createElement("button");
+            $okButton.innerHTML = "Yes";
+            $okButton.onclick = () => {
+                this.dialogContainer.classList.add("hidden");
+                this.dialogContainer.innerHTML = "";
+                resolve(true);
+            }
+            const $cancelButton = document.createElement("button");
+            $cancelButton.innerHTML = "No";
+            $cancelButton.onclick = () => {
+                this.dialogContainer.classList.add("hidden");
+                this.dialogContainer.innerHTML = "";
+                resolve(false);
+            }
+            $newActions.appendChild($okButton);
+            this.mainInnerDialog.appendChild($newContent);
+            this.mainInnerDialog.appendChild($newActions);
+            this.dialogContainer.appendChild(this.mainInnerDialog);
+        })
+    }
+
+    /**
+     * Opens the menu
+     */
+    open() {
+        this.menuContainer.classList.remove("hidden");
+    }
+
+    /**
+     * Closes the menu
+     */
+    close() {
+        this.menuContainer.classList.add("hidden");
+    }
+
+    /**
+     * Toggles the visibility of the menu
+     */
+    toggle() {
+        this.menuContainer.classList.toggle("hidden");
+    }
+
+    /**
+     * Selects the currently selected item as if the user had clicked on it
+     */
+    select() {
+        //this.mainInnerSection.querySelector("tvm-item.selected").click();
+    }
+
+    /**
+     * Removes the menu from the DOM. This is useful when the menu is no
+     * longer needed and you want to free up resources.
+     */
+    destroy() {
+        this.menuContainer.remove();
+        this.mainInnerSection.remove();
+        this.dialogContainer.remove();
+        this.mainInnerDialog.remove();
     }
 }
 
 class TVMenuItem {
-  /**
-   * Constructor for TVMenuItem
-   * @param {object} options - Options for the TVMenuItem
-   * @param {string} options.text - The text to display for the item
-   * @param {string} options.icon - Valid HTML to display as an icon. Can be a basic icon or even an image.
-   * @param {string} options.type - The type of item. Must be one of: button, checkbox, enum, folder, or color
-   * @param {function} [options.onSelect] - The function to call when the item is selected
-   * @param {{text: string, value: string}[]} [options.possibleValues] - An array of valid options for the enum type. Only used if type is enum.
-   * @param {function(string)} [options.onChange] - The function to call when the item's value changes. Won't be called if type is button or folder.
-   * @param {TVMenuItem[]} [options.children] - The array of children items for this item. Only used if type is folder.
-   * @param {boolean} [options.allowAlpha] - Whether or not the item should allow alpha. Only used if type is color.
-   * @param {boolean} [options.isPassword] - Whether or not the item should be a password. Only used if type is input.
-   * @param {string | number} [options.default]
-   * The default value of the item. Optional, but won't be used if type is button or folder.
-   * This property must evaluate to a CSS color if type is color. Can be a string if type is input.
-   * Must evaluate to a bool if type is checkbox.
-   */
-  constructor(options) {
-    this.text = options.text;
-    if (
-      options.type !== "button" &&
-      options.type !== "checkbox" &&
-      options.type !== "enum" &&
-      options.type !== "folder" &&
-      options.type !== "color" &&
-      options.type !== "input" &&
-      options.type !== "separator"
-    ) {
-      throw new Error("TVMenuItem: type is invalid.");
-    } else if (
-      (options.type === "enum" && (!options.possibleValues || !options.default)) ||
-      options.possibleValues.length === 0
-    ) {
-      throw new Error("TVMenuItem: possibleValues is invalid.");
+    /**
+     * Constructor for TVMenuItem
+     * @param {object} options - Options for the TVMenuItem
+     * @param {string} [options.text] - The text to display for the item
+     * @param {string} [options.icon] - Valid HTML to display as an icon. Can be a basic icon or even an image.
+     * @param {string} options.type - The type of item. Must be one of: button, checkbox, enum, folder, input, number, or separator
+     * @param {function(Event)} [options.onSelect] - The function to call when the item is selected
+     * @param {{text: string, value: string}[]} [options.possibleValues] - An array of valid options for the enum type. Only used if type is enum.
+     * @param {function(string)} [options.onChange] - The function to call when the item's value changes. Won't be called if type is button or folder.
+     * @param {TVMenuItem[]} [options.children] - The array of children items for this item. Only used if type is folder.
+     * @param {boolean} [options.isPassword] - Whether or not the item should be a password. Only used if type is input.
+     * @param {number} [options.min] - The minimum value of the item. Only used if type is number.
+     * @param {number} [options.max] - The maximum value of the item. Only used if type is number.
+     * @param {string|number} [options.default]
+     * The default value of the item. Optional, but won't be used if type is button or folder.
+     * This property must evaluate to a CSS color if type is color. Can be a string if type is input.
+     * Must evaluate to a bool if type is checkbox.
+     */
+    constructor(options) {
+        this.text = options.text;
+        if (
+            options.type !== "button" &&
+            options.type !== "checkbox" &&
+            options.type !== "enum" &&
+            options.type !== "number" &&
+            options.type !== "folder" &&
+            options.type !== "input" &&
+            options.type !== "separator"
+        ) {
+            throw new Error("TVMenuItem: type is invalid.");
+        } else if (
+            (options.type === "enum" &&
+                (!options.possibleValues || !options.default)) ||
+            options.possibleValues.length === 0
+        ) {
+            throw new Error("TVMenuItem: possibleValues or default is invalid.");
+        } else if (
+            options.type === "number" && (!min || !max)
+        ) {
+            console.warn(
+              "TVMenuItem: min or max is missing, input type will be number instead of range"
+            );
+        } else if (
+            options.type === "folder" && (!options.children || options.children.length === 0)
+        ) {
+            throw new Error("TVMenuItem: children is null or empty.")
+        }
+        this.type = options.type;
+        this.icon = options.icon;
+        this.onSelect = options.onSelect;
+        this.possibleValues = options.possibleValues;
+        this.onChange = options.onChange;
+        this.children = options.children;
+        this.isPassword = options.isPassword;
+        this.min = options.min;
+        this.max = options.max;
+        this.default = options.default;
+        this.value = this.default;
+
+        this.domElement = document.createElement("tvm-item");
     }
-    this.type = options.type;
-    this.icon = options.icon;
-    this.onSelect = options.onSelect;
-    this.possibleValues = options.possibleValues;
-    this.onChange = options.onChange;
-    this.children = options.children;
-    this.allowAlpha = options.allowAlpha;
-    this.isPassword = options.isPassword;
-    this.default = options.default;
-    this.value = "";
-  }
+
+    /**
+     * Evaluates a string to determine if it represents a boolean true value.
+     *
+     * @param {string} str - The string to evaluate.
+     * @returns {boolean} - Returns true if the string is "true" or "1", otherwise false.
+     */
+    evaluateBool(str) {
+        return str == "true" || str == "1";
+    }
 }
